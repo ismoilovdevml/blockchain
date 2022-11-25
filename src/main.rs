a39989bba6d0eb4ea892dbc0622915019f7cb5fc
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-25 10:21:09 +0500

    in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Block {
+    pub id: u64,
+    pub hash: String,
+    pub previous_hash: String,
+    pub timestamp: i64,
+    pub data: String,
+    pub nonce: u64
 }
