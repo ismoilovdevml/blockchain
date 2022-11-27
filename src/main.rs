1689b21abaeeed4ca5b27d3c9ac57a1ef923e259
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-11-27 19:58:01 +0500

    libp2p

--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,21 @@ pub struct Block {
     pub data: String,
     pub nonce: u64
 }
+
+// libp2p client yaratish
+#[tokio::main]
+aysnc fn main() {
+    pretty_env_logger::init();
+
+    info!("Peer ID: {}", PEER_ID.clone());
+    let (respomse_sender, mut respomse_rcv) = mpsc::unbounded_channel();
+    let auth_keys = Keypair::<X25519Spec>::new()
+        .into_authentic(&KEYS)
+        .expect("Autentifikatsiya kalitlarini yaratish")
+
+    let transp = TokioTcpConfig::new()
+        .upgrade(upgrade::Version::V1)
+        .authenticate(NoiseConfig::xx(auth_keys).into_authentic)
+        .multiplex(mplex::MpexConfig::new())
+        .boxed();
+}
