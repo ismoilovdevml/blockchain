420e537dca53d739db9f7dba3b86d07c3aa29d06
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2022-12-01 21:51:57 +0500

    chain selection

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ impl App {
         }
         true
     }
-    fn is_chain_valid(&self, chain; &[Block]) -> bool {
+    fn is_chain_valid(&self, chain: &[Block]) -> bool {
         for i in 0..chain.len() {
             if i == 0 {
                 continue;
@@ -83,5 +83,24 @@ impl App {
             }
         }
         true
+    }
+    // zanjir tanlash
+    fn choose_chain(&mut self, local: Vec<Block>, remote: Vec<Block>) -> Vec<Block> {
+        let is_local_valid = self.is_chain_valid(&local);
+        let is_remote_valid = self.is_chain_valid(&remote);
+
+        if is_local_valid && is_remote_valid {
+            if local.len() >= remote.len() {
+                local
+            } else {
+                remote
+            }
+        } else if is_remote_valid && !is_local_valid {
+            remote
+        } else if !is_remote_valid && is_local_valid {
+            local
+        } else {
+            panic!("yaroqsiz local va uzun zanjirlar")
+        }
     }
 }
